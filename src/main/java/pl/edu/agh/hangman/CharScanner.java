4bb12585c23efce402a43aeb3ea79a95edf415b1
radekkuc <radoslaw.kuc@op.pl>
2023-02-12 10:17:14 +0100

    class reading a char from user

---
+++ src/main/java/pl/edu/agh/hangman/CharScanner.java
@@ -0,0 +1,25 @@
+package pl.edu.agh.hangman;
+
+
+import java.util.Scanner;
+
+public class CharScanner {
+
+    public static void main(String[] args) {
+
+        String scanner = new CharScanner().getChar();
+    }
+
+    public String getChar() {
+        System.out.println("Please enter your char");
+        Scanner scan = new Scanner(System.in);
+        String userChar = scan.next();
+        while (userChar.length() != 1) {
+            System.out.println("Try again");
+            userChar = scan.next();
+        }
+
+        System.out.println("This is your char " + userChar);
+        return userChar;
+    }
+}
